d0f3d2af88ffa45f1a6d0361be48803d8f44d450
Swati Singh <swsingh.in@gmail.com>
2021-03-22 12:20:22 +0100

    add styled-component for header

---
+++ src/components/styles/Header.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Input = styled.header`
+  align-items: center;
+  color: #FFF;
+  display: flex;
+  padding: 16px 0 40px;
+
+  img {
+    margin-right: 10px;
+  }
+`;
+
+export default Input;
