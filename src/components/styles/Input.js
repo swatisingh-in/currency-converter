76e82b19d6c2109ce1bec325af7adee0c811190c
Swati Singh <swsingh.in@gmail.com>
2021-03-22 12:03:14 +0100

    add a styled-component for input tag

---
+++ src/components/styles/Input.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  background: #FAFAFA;
+  border: 1px solid #8FA1C0;
+  box-sizing: border-box;
+  border-radius: 4px;
+  padding: 8px;
+  margin-right: 20px;
+  width: 50%;
+
+  &:focus {
+    outline-color: #2484ff;
+  }
+`;
+
+export default Input;
