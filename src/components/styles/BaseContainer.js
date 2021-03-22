9ba803bbe0d5e6ffb4e856cd457397c87d80662e
Swati Singh <swsingh.in@gmail.com>
2021-03-22 12:05:28 +0100

    style div containers

---
+++ src/components/styles/BaseContainer.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const BaseContainer = styled.section`
+  padding: 24px;
+`;
+
+export default BaseContainer;
---
+++ src/components/styles/ConversionContainer.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import BackGroundImage from '../../images/background.jpeg';
+
+const ConversionContainer = styled.section`
+  background: linear-gradient(180deg, #000000 0%, rgba(34, 34, 34, 0.38) 100%), url(${BackGroundImage});
+  box-shadow: 0px 3px 11px 4px rgba(0, 0, 0, 0.26);
+  border-radius: 10px;
+  padding: 24px 50px 50px;
+`;
+
+export default ConversionContainer;
