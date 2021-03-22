fc8027cd6c11c07877417fd0c26a1b81287432a0
Swati Singh <swsingh.in@gmail.com>
2021-03-22 17:05:43 +0100

    add styled-component for graph container.

---
+++ src/components/styles/GraphContainer.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const GraphContainer = styled.section`
+  background: linear-gradient(0deg, #FFFFFF, #FFFFFF);
+  box-shadow: 0px 3px 11px 4px rgba(0, 0, 0, 0.26);
+  border-radius: 10px;
+  margin: 50px;
+  padding: 32px;
+`;
+
+export default GraphContainer;
